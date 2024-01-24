53b7a15ded30f6c50cb82340e014ce0b3e7cbdf1
Shourya Pratap Singh <93045361+amspsingh04@users.noreply.github.com>
2024-01-24 17:20:53 +0530

    .

---
+++ ips3/demo.js
@@ -0,0 +1,26 @@
+
+var string='Hi'
+function reverse(str){
+let newString = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+function fact(num){
+let fact = readline.question("Enter number: ")
+for(let i=1;i<=num;i++){
+fact*=i
+}
+return fact;
+}
+function palin(str){
+if(str == reverse(str)){
+return "Palindrome"
+}
+else
+return "Not a Palindrome"
+}
+console.log(reverse(string))
+console.log(fact(5))
+console.log(palin('MOM'))
