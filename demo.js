1107c77d73fdfb9b33ed267d668b7afb313610e5
amspsingh04 <sp.singh.150604@gmail.com>
2024-01-25 23:40:30 +0530

    .

--- demo.js
+++ demo.js
@@ -43,10 +43,23 @@ function printNumbers() {
         }
     }
 }
-
-
+function arraySum(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+function findMissingNumber(arr) {
+    let n = arr.length + 1; // n is one more than the length of the array (due to missing number)
+    let expectedSum = (n * (n + 1)) / 2; // Sum of all numbers from 0 to n
+    let actualSum = arr.reduce((acc, num) => acc + num, 0); // Sum of all numbers in the array
+    return expectedSum - actualSum; // The difference is the missing number
+}
 console.log(reverse(string))
 console.log(fact(5))
 console.log(palin('MOM'))
 console.log(findMax([10,20,5,8,9]));
 printNumbers();
+console.log(arraySum([1,2,3,4,5])); 
+console.log(findMissingNumber([1,2,3,4,5,7,8,9,10]));
